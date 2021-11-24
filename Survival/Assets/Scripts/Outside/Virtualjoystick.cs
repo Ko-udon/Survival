e98c62fff58d48a20ba19e43e037b6681ec4f892
Swanq <45062141+snr1229@users.noreply.github.com>
2021-11-24 20:04:53 +0900

    조이스틱 추가

---
+++ Survival/Assets/Scripts/Outside/Virtualjoystick.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems; //Support sending input of keyboard, Mouse, touch as Event to object
+
+public class Virtualjoystick : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    [SerializeField]
+    private RectTransform lever;
+    private RectTransform rectTransform;
+
+    [SerializeField, Range(10, 150)]
+    private float leverRange;
+
+    private Vector2 inputDirection;
+    private bool isInput;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        ControlJoystickLever(eventData);
+        isInput = true;
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        ControlJoystickLever(eventData);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        lever.anchoredPosition = Vector2.zero;
+        isInput = false;
+    }
+
+    private void ControlJoystickLever(PointerEventData eventData)
+    {
+        var inputPos = eventData.position - rectTransform.anchoredPosition;
+        var inputVector = inputPos.magnitude < leverRange ? inputPos : inputPos.normalized * leverRange;
+        lever.anchoredPosition = inputVector;
+        inputDirection = inputVector / leverRange;
+    }
+
+    private void InputControlVector()
+    {
+        Debug.Log(inputDirection.x + " . " + inputDirection.y);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isInput)
+        {
+            InputControlVector();
+        }
+    }
+}
